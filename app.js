b73977c89ea151776f694f048ffd4de6e8c08aec
Leevi Alho <54808340+LeeviAlho@users.noreply.github.com>
2020-12-31 17:01:35 +0200

    vers1.0.2

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
-var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 var session = require("express-session");
 
@@ -27,7 +26,6 @@ app.set("view engine", "pug");
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser("maanantaiaamut"));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.set("trust proxy", 1);
